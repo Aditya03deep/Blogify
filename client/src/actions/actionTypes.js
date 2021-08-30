ae6d4d9f39b7be3d825633e6d5241b562bd3d8d7
ADITYA DEEPAK <69453161+Aditya03deep@users.noreply.github.com>
2021-08-30 10:17:26 +0530

    Delete client directory

--- client/src/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const FETCH_ALL = 'FETCH_ALL';
-export const DELETE = 'DELETE';
-export const LIKE = 'LIKE'
-export const AUTH = 'AUTH'
-export const LOGOUT = 'LOGOUT'
